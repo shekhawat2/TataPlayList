1d324280b0465fe9a949258484b0087a886ac3e2
Mahipat Chauhan <mahipat5678@gmail.com>
2024-01-16 13:23:40 +0530

    typo

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
 
 import fetch, { Headers } from "cross-fetch";
 import { replacestrings } from './stringreplace';
-const { filterChannels } = require('./channelFilter');
+const { filterChannels } = require('./channelfilter');
 // const baseUrl = "https://kong-tatasky.videoready.tv";
 const baseUrl = "https://tm.tapi.videoready.tv";
 
